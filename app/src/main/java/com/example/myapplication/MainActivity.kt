fea2c5f80b9561db8cd5f143e745048da40e4eaa
MarkMallari16 <mallariftw01@gmail.com>
2025-01-10 00:45:14 +0800

    Make the input dynamic

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
-                    Greeting("Mark Mallari")
+                    UserInputScreen()
 
                 }
             }
@@ -37,11 +45,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Green) {
-        Text(
-            text = "Hi, My name is $name!",
-            modifier = modifier.padding(24.dp)
+    Surface(color = Color.White) {
+
+
+    }
+}
+@Composable
+fun UserInputScreen(){
+    var userInput by remember { mutableStateOf("") }
+    var displayText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        TextField(
+            value = userInput,
+            onValueChange = { userInput = it },
+            label = { Text("Enter your name") },
+            modifier = Modifier.fillMaxWidth()
         )
+
+        Button(
+            onClick = {displayText = "Hello, $userInput!"},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Submit")
+        }
+
+        if (displayText.isNotEmpty()){
+            Text(
+                text = displayText,
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.Blue
+            )
+        }
     }
 }
 @Composable
